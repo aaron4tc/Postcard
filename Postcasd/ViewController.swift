5b488e13d56284cbf6aab7695a73d334dc4bf7aa
Aaron Chen <aaron@innotec.com.au>
2015-03-06 16:12:58 +1100

    Initial Commit

---
+++ Postcasd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Postcasd
+//
+//  Created by Zhongjie Chen on 6/03/2015.
+//  Copyright (c) 2015 Zhongjie Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
